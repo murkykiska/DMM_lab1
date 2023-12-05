402060da107cf1ec3faacb4afc0fbf8011900d47
.murky kiska <catmasya97@gmail.com>
2023-12-05 07:24:49 +0700

    TODO: Решение СЛАУ с 1 уравнением

--- DMM_lab1/Program.cs
+++ DMM_lab1/Program.cs
@@ -7,7 +7,7 @@ namespace DMM_lab1
         static string path = "D:\\ACS\\Study\\Дискретные матмодели\\DMM_lab1\\DMM_lab1\\Tests\\";
         static void Main(string[] args)
         {
-            SLAE s = new SLAE(path + "test2.txt");
+            SLAE s = new SLAE(path + "test1.txt");
             s.ConvertToGeneralSolution();
         }
     }
